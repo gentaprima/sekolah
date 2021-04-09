00bebf1f8f887bcfd7dd1f1a423cba57f588f575
bmhrwn <bmhrwn@gmail.com>
2021-04-10 02:06:15 +0700

    update

--- application/views/home/data/pendaftaran.php
+++ application/views/home/data/pendaftaran.php
@@ -11,32 +11,6 @@
         </div>
     </div>
 </header>
-<<<<<<< HEAD
-    <div class="section-padding">
-        <div class="container">
-            <div class="row">
-            <center><h4>Silahkan isi halaman form pendaftaran sesuai ketentuan yang ada.</h4></center>
-                <div class="col-md-6">
-                    <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label">Nama Lengkap</label>
-                        <div class="col-sm-9">
-                            <input type="text" value="" id="" name="full_name" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label">Tanggal Lahir</label>
-                        <div class="col-sm-9">
-                            <input type="text" value="" id="" name="tgl_lahir" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label">Alamat</label>
-                        <div class="col-sm-9">
-                            <input type="text" value="" id="" name="alamat" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-=======
 <div class="section-padding">
     <div class="container">
         <div class="data-1" id="data_1">
@@ -72,30 +46,10 @@
                 </div>
                 <div class="col-md-6">
                 <div class="form-group row">
->>>>>>> dcea7f7aaac5c56bddcc1be0e9b08672fbfa5412
                         <label for="" class="col-sm-3 col-form-label">Nama Orang Tua</label>
                         <div class="col-sm-9">
                             <input type="text" value="" id="" name="nama_orangtua" class="form-control">
                         </div>
-<<<<<<< HEAD
-                    </div>
-
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label">Jenis Kelamin</label>
-                        <div class="col-sm-9">
-                            <input type="text" value="" id="" name="jenis_kelamin" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="formFile" class="col-sm-3 col-form-label">Ijazah</label>
-                        <div class="col-sm-9">
-                            <input class="form-control" type="file" name="" id="formFile">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-=======
                     </div>
                     <div class="form-group row">
                         <label for="" class="col-sm-3 col-form-label">Jenis Kelamin</label>
@@ -168,24 +122,17 @@
                 <div class="col-md-6 mt-4">
                     
                 <div class="form-group row">
->>>>>>> dcea7f7aaac5c56bddcc1be0e9b08672fbfa5412
                         <label for="formFile" class="col-sm-3 col-form-label">Akte Kelahiran</label>
                         <div class="col-sm-9">
                             <input class="form-control" type="file" name="" id="formFile">
                         </div>
                     </div>
                     <div class="form-group row">
-<<<<<<< HEAD
-                        <label for="formFile" class="col-sm-3 col-form-label">Kartu Remi</label>
-=======
                         <label for="formFile" class="col-sm-3 col-form-label">Kartu Keluarga</label>
->>>>>>> dcea7f7aaac5c56bddcc1be0e9b08672fbfa5412
                         <div class="col-sm-9">
                             <input class="form-control" type="file" name="" id="formFile">
                         </div>
                     </div>
-<<<<<<< HEAD
-=======
                 </div>
                 <div class="col-md-6">
                     
@@ -196,20 +143,11 @@
                         </div>
                     </div>
                   
->>>>>>> dcea7f7aaac5c56bddcc1be0e9b08672fbfa5412
 
                 </div>
             </div>
             <div class="d-grid gap-2 d-md-block">
                 <center>
-<<<<<<< HEAD
-                <button style="font-size: 20px;" class="btn btn-primary" type="button">Simpan</button>
-                <button style="font-size: 20px;" class="btn btn-warning" type="button">Reset</button>
-                </center>
-            </div>
-        </div>
-    </div>
-=======
                     <button  class="btn btn-primary" type="button">Daftar</button>
                     <button  class="btn btn-outline-danger" onClick="back(3)" type="button">Kembali</button>
                 </center>
@@ -244,4 +182,3 @@
         }
     }
 </script>
->>>>>>> dcea7f7aaac5c56bddcc1be0e9b08672fbfa5412
