bcc6dab3258d26ed92e651a1412731c1421c50a8
rofiikhsan68 <rofii.saputra@gmail.com>
2021-04-09 23:25:38 +0700

    Pendaftaran

---
+++ application/views/home/data/pendaftaran.php
@@ -0,0 +1,80 @@
+<header class="site-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <h1 class="white-color">Welcome To Mobile Legend</h1>
+                <ul class="breadcrumb">
+                    <li><a href="index-2.html">Home</a></li>
+                    <li>Pendaftaran</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="section-padding">
+    <div class="container">
+        <div class="row">
+        <center><h4>Silahkan isi halaman form pendaftaran sesuai ketentuan yang ada.</h4></center>
+            <div class="col-md-6">
+                <div class="form-group row">
+                    <label for="" class="col-sm-3 col-form-label">Nama Lengkap</label>
+                    <div class="col-sm-9">
+                        <input type="text" value="" id="" name="full_name" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="" class="col-sm-3 col-form-label">Tanggal Lahir</label>
+                    <div class="col-sm-9">
+                        <input type="text" value="" id="" name="tgl_lahir" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="" class="col-sm-3 col-form-label">Alamat</label>
+                    <div class="col-sm-9">
+                        <input type="text" value="" id="" name="alamat" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="" class="col-sm-3 col-form-label">Nama Orang Tua</label>
+                    <div class="col-sm-9">
+                        <input type="text" value="" id="" name="nama_orangtua" class="form-control">
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+                <div class="form-group row">
+                    <label for="" class="col-sm-3 col-form-label">Jenis Kelamin</label>
+                    <div class="col-sm-9">
+                        <input type="text" value="" id="" name="jenis_kelamin" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="formFile" class="col-sm-3 col-form-label">Ijazah</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" type="file" name="" id="formFile">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="formFile" class="col-sm-3 col-form-label">Akte Kelahiran</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" type="file" name="" id="formFile">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="formFile" class="col-sm-3 col-form-label">Kartu Remi</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" type="file" name="" id="formFile">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="d-grid gap-2 d-md-block">
+            <center>
+            <button style="font-size: 20px;" class="btn btn-primary" type="button">Simpan</button>
+            <button style="font-size: 20px;" class="btn btn-warning" type="button">Reset</button>
+            </center>
+        </div>
+    </div>
+</div>
