78c903f5e4cfacf8be0e4cbbc5c08b56a79945f1
bmhrwn <bmhrwn@gmail.com>
2021-04-18 02:53:16 +0700

    Proses Daftar User (Bima)

--- application/views/home/layout/navbar.php
+++ application/views/home/layout/navbar.php
@@ -94,11 +94,12 @@
                         <div class="content registerBox" style="display:none;">
                             <div class="form">
                             <!-- REGISTER -->
-                                <form method="" action="" accept-charset="UTF-8">
+                                <form method="post" action="<?= base_url()?>login/procces_register" accept-charset="UTF-8">
                                     <input id="username" class="form-control" type="text" placeholder="Username" name="username">
+                                    <input id="email" class="form-control" type="text" placeholder="Email" name="email">
                                     <input id="password" class="form-control" type="password" placeholder="Password" name="password">
                                     <input id="password_confirmation" class="form-control" type="password" placeholder="Repeat Password" name="password_confirmation">
-                                    <input class="btn btn-default btn-register" type="button" value="Create account" name="commit">
+                                    <input class="btn btn-default btn-register" type="submit" value="Create account" name="commit">
                                 </form>
                             </div>
                         </div>
