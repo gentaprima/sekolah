b07083dbeabec3c91aa35afd401efba1e1f356ed
gentaprima <gentaprima600@gmail.com>
2021-05-21 23:56:11 +0700

    update

--- application/views/dashboard/data/verifikasi_student.php
+++ application/views/dashboard/data/verifikasi_student.php
@@ -1,5 +1,6 @@
 <div class="breadcome-area">
     <div class="container-fluid">
+    
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="breadcome-list single-page-breadcome">
