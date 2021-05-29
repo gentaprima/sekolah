69efe53265f621f7b95e7519e574586b785681dd
rofiikhsan68 <rofii.saputra@gmail.com>
2021-05-29 10:44:16 +0700

    mao kelar sumpah dah

--- application/models/ModelSiswa.php
+++ application/models/ModelSiswa.php
@@ -24,8 +24,18 @@ class ModelSiswa extends CI_Model
                 tbl_kuota.id_kelas = tbl_kelas.id_kelas order by tbl_pendaftaran.id_pendaftaran DESC ";
         return $this->db->query($sql)->result_array();
     }
-    public function getDataVerifikasiSiswaTidakLolos()
+    public function getDataVerifikasiSiswaLolos()
     {
+        $sql = "SELECT * FROM tbl_pendaftaran,tbl_siswa,tbl_user,tbl_kuota,tbl_kelas
+        WHERE
+        tbl_siswa.id_user = tbl_user.id_user and
+        tbl_pendaftaran.nis = tbl_siswa.nis and
+        tbl_pendaftaran.id_kelas = tbl_kelas.id_kelas and
+        is_verify = 1 and
+        tbl_kuota.id_kelas = tbl_kelas.id_kelas order by tbl_pendaftaran.id_pendaftaran DESC ";
+        return $this->db->query($sql)->result_array();
+    }
+    public function getDataVerifikasiSiswaTidakLolos(){
         $sql = "SELECT * FROM tbl_pendaftaran,tbl_siswa,tbl_user,tbl_kuota,tbl_kelas
         WHERE
         tbl_siswa.id_user = tbl_user.id_user and
